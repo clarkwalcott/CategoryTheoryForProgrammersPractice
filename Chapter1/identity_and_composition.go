32571668531c4b937571d3647904f4830dc1686b
Clark Walcott <gamewiz365@Aol.com>
2022-07-09 16:41:59 -0500

    add identity and composition challenges

---
+++ Chapter1/identity_and_composition.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+type F func(x interface{}) interface{}
+
+func main() {
+	myFunc := func(x interface{}) interface{} { return "abcd" }
+
+	// Expect that id . f === f . id == f
+	// Output should be abcd, not
+	fmt.Println(compose(id, myFunc)("input"))
+	fmt.Println(compose(myFunc, id)("input"))
+}
+
+func id(x interface{}) interface{} {
+	return x
+}
+
+func compose(f, g F) F {
+	return func(x interface{}) interface{} { return f(g(x)) }
+}
